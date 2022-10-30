ac431e115972836417fe0e19447668c89f734fc8
develoeprboy1990 <62117143+develoeprboy1990@users.noreply.github.com>
2022-10-30 09:22:11 +0500

    remove register link from admin login

--- resources/views/user/admin/login.blade.php
+++ resources/views/user/admin/login.blade.php
@@ -80,51 +80,11 @@
             </div>
             <div class="clearfix"></div>
             <div class="separator">
-
-              <p class="change_link">New to site?
-                <a href="#toregister" class="to_register"> Create Account </a>
-              </p>
               <div class="clearfix"></div>
               <br />
               <div>
                 <h1><img src="{{url('public/uploads/logo.png')}}"></h1>
                 <p>©2022 All Rights Reserved. dartsinabottle. Privacy and Terms</p>
-              </div>
-            </div>
-          </form>
-          <!-- form -->
-        </section>
-        <!-- content -->
-      </div>
-      <div id="register" class="animate form">
-        <section class="login_content">
-          <form>
-            <h1>Create Account</h1>
-            <div>
-              <input type="text" class="form-control" placeholder="Username" required="" />
-            </div>
-            <div>
-              <input type="email" class="form-control" placeholder="Email" required="" />
-            </div>
-            <div>
-              <input type="password" class="form-control" placeholder="Password" required="" />
-            </div>
-            <div>
-              <a class="btn btn-default submit" href="index.html">Submit</a>
-            </div>
-            <div class="clearfix"></div>
-            <div class="separator">
-
-              <p class="change_link">Already a member ?
-                <a href="#tologin" class="to_register"> Log in </a>
-              </p>
-              <div class="clearfix"></div>
-              <br />
-              <div>
-                <h1><img src="{{url('public/uploads/logo.png')}}"></h1>
-
-                <p>©2022 All Rights Reserved. dartsinabottle. Privacy and Terms</p>
-               
               </div>
             </div>
           </form>
