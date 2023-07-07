519d6efd809eca402e8815c6fa3e07eb446fdf7b
Mutahir Shah <mutahiricup@gmail.com>
2023-07-07 10:29:40 +0500

    dbfileupload

---
+++ app/Models/User.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Cashier\Billable;
+use App\Subscription;
+
+class User extends Authenticatable
+{
+    use Notifiable,Billable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','stripe_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public  function  getCustomerGroup(){
+
+        return $this->belongsTo('App\CustomerGroup','customer_group_id','id');
+      
+      // $customer_group=  CustomerGroup::where('id',$id)->value('name');
+       
+      // return $customer_group; 
+    }
+
+    public  function  getCountry(){
+    
+      return $this->belongsTo('App\Country','country_id','id');
+      
+    }
+
+     public  function  getState(){
+    
+      return $this->belongsTo('App\State','state_id','id');
+      
+    }
+
+     public  function  getCity(){
+    
+      return $this->belongsTo('App\City','city_id','id');
+      
+    }
+
+    public  function  check_deporit($user_id){
+
+        $user_supcription = Subscription::where('user_id',$user_id)->orderBy('id', 'desc')->first();
+
+        if($user_supcription->choice == 'Lend')
+        {
+            return 40;
+        }else{
+            return 50;
+        }
+      
+    }
+
+    
+
+
+}
